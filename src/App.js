aa434f06429ae1c13f1c0651ab2d57284ad54f76
parksujin <qkrtnwls1711@naver.com>
2020-04-15 20:42:49 +0900

    onInsert

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useCallback, useRef, useState } from 'react';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
@@ -22,10 +22,25 @@ function App() {
     },
   ]);
 
+  //id가 4부터 추가되기 때문
+  const nextId = useRef(4);
+  const onInsert = useCallback(
+    (text) => {
+      const todo = {
+        id: nextId.current,
+        text,
+        checked: false,
+      };
+      setTodos(todos.concat(todo));
+      nextId.current += 1;
+    },
+    [todos]
+  );
+
   return (
     <div className="App">
       <TodoTemplate>
-        <TodoInsert />
+        <TodoInsert onInsert={onInsert} />
         <TodoList todos={todos} />
       </TodoTemplate>
     </div>
--- src/components/TodoInsert.js
+++ src/components/TodoInsert.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import './TodoInsert.scss';
 import { MdAdd } from 'react-icons/md';
 
-const TodoInsert = () => {
+// props로 부터 onInsert를 props로 받음
+const TodoInsert = ({ onInsert }) => {
+  const [value, setValue] = useState('');
+
+  const onChange = useCallback((e) => {
+    setValue(e.target.value);
+  }, []);
+
+  const onSubmit = useCallback(
+    (e) => {
+      onInsert(value);
+      setValue('');
+      e.preventDefault();
+    },
+    [onInsert, value]
+  );
+
+  //input의 value,onChange가 포인트
   return (
-    <form className="TodoInsert">
-      <input type="text" placeholder="todo를 입력하세요" />
+    <form className="TodoInsert" onSubmit={onSubmit}>
+      <input
+        type="text"
+        placeholder="todo를 입력하세요"
+        value={value}
+        onChange={onChange}
+      />
       <button type="submit">
         <MdAdd />
       </button>
