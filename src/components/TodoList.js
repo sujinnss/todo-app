48f653459bf141cb83323e0b7fba8013ee9e91e9
parksujin <qkrtnwls1711@naver.com>
2020-04-21 17:39:27 +0900

    #11

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import './TodoList.scss';
 import TodoItem from './TodoItem';
-import TodoResult from './TodoResult';
-import TodoTemplate from './TodoTemplate';
 
-const TodoList = ({ todos, onRemove, onToggle, onToggleStar }) => {
-  return (
+const TodoList = ({ todos, onRemove, onToggle, onToggleStar,onTodoSort }) => {
+
+
+    return (
     <div className="TodoList">
-      {todos.map((todo) => (
-        <TodoItem
-          todo={todo}
-          key={todo.id}
-          onRemove={onRemove}
-          onToggle={onToggle}
-          onToggleStar={onToggleStar}
-        />
-      ))}
+       {
+           todos.sort(onTodoSort).map((todo) => (
+           <TodoItem
+           todo={todo}
+           key={todo.id}
+           onRemove={onRemove}
+           onToggle={onToggle}
+           onToggleStar={onToggleStar}
+         />
+       ))
+       }
     </div>
   );
 };
--- src/components/TodoResult.js
+++ src/components/TodoResult.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import './TodoResult.scss';
-import {
-  MdSentimentVerySatisfied,
-  MdSentimentVeryDissatisfied,
-} from 'react-icons/md';
 
 const TodoResult = ({todos}) => {
-
   return (
     <div className="TodoResult">
       <div className="complete">완료 :{todos.filter(todo => todo.checked).length}</div>
