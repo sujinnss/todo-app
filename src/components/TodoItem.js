c86c3e754c6a44e605915e92189a870070cd3222
jinha <gojinha@naver.com>
2020-04-24 20:18:57 +0900

    animation 추가

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -8,11 +8,14 @@ import {
 } from 'react-icons/md';
 import cn from 'classnames';
 import './TodoItem.scss';
+import { fromJS, is } from 'immutable';
+import Anime from 'react-anime';
 
 const TodoItem = ({ todo, onRemove, onToggle, onToggleStar }) => {
     const { id, text, checked, star, date } = todo;
 
     return (
+        <Anime opacity={[0, 1]} translateX={[-250, 0]}>
         <div className="TodoItem">
             <div
                 className={cn('checkbox', { checked })}
@@ -37,7 +40,15 @@ const TodoItem = ({ todo, onRemove, onToggle, onToggleStar }) => {
                 <MdRemove />
             </div>
         </div>
+        </Anime>
     );
 };
 
-export default TodoItem;
+export default React.memo(TodoItem, (prevProps, nextProps) => {
+    // 다르면 memo를 하지 않음
+    if (!is(fromJS(prevProps.todo), fromJS(nextProps.todo))) {
+        return false
+    }
+    // 같으면 memo
+    return true
+});
