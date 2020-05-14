7a58a2ca8cbaccc6abed9c2bf839a7d51d0d950a
parksujin <qkrtnwls1711@naver.com>
2020-05-14 21:37:04 +0900

    #36  TodoTemplete 구조 수정

--- src/App.js
+++ src/App.js
@@ -4,9 +4,8 @@ import TodoListTemplate from './components/TodoListTemplate';
 import { ColorProvider } from './contexts/color';
 import { Layout, Menu } from 'antd';
 import { SmileOutlined, StarOutlined, CopyOutlined } from '@ant-design/icons';
-import { Link } from 'react-router-dom';
+import { Link, Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 // import Route from 'react-router-dom/es/Route';
-import { Route } from 'react-router-dom';
 import TodoStarItem from './components/TodoStarItem';
 import SelectColor from './components/SelectColor';
 
@@ -14,98 +13,93 @@ function App() {
     const { Sider } = Layout;
     const [sider, setSider] = useState(false);
 
-    // const title = '';
+    // 저장 돼 있으면
+    let initAllDatas;
+    if (localStorage.getItem('allDatas')) {
+        initAllDatas = JSON.parse(localStorage.getItem('allDatas'));
+    } else {
+        initAllDatas = [
+            { key: '/', title: '할일', todos: [] },
+            { key: 'star', title: '중요', todos: [] },
+            { key: 'ex', title: '예시', todos: [] },
+        ];
+        localStorage.setItem('allDatas', JSON.stringify(initAllDatas));
+    }
+    const [allDatas, setAllDatas] = useState(initAllDatas);
 
     const onCollapse = (sider) => {
         console.log(sider);
         setSider(sider);
     };
-    // TODO refresh 할 경우 라우터 수정
-    // console.log('App Rendered');
-    // defaultSelectedKeys={['1']} : Menu.Item의 key=1을 선택한 상태로 시작
+
+    const saveAll = (allDatas) => {
+        setAllDatas(allDatas);
+        localStorage.setItem('allDatas', JSON.stringify(allDatas));
+    };
+
     return (
-        <ColorProvider>
-            <Layout
-                style={{ minHeight: '100vh' }}
-                onClick={(e) => {
-                    console.log('App clicked! popup false');
-                }}
-            >
-                <Sider
-                    collapsible
-                    collapsed={sider}
-                    onCollapse={onCollapse}
-                    style={{ width: '600px' }}
+        <Router>
+            <ColorProvider>
+                <Layout
+                    style={{ minHeight: '100vh' }}
+                    onClick={(e) => {
+                        console.log('App clicked! popup false');
+                    }}
                 >
-                    <div className="logo" />
-                    <Menu
-                        theme="dark"
-                        defaultSelectedKeys={['1']}
-                        mode="inline"
+                    <Sider
+                        collapsible
+                        collapsed={sider}
+                        onCollapse={onCollapse}
+                        style={{ width: '600px' }}
                     >
-                        <Menu.Item
-                            key="1"
-                            style={{
-                                margin: '0px',
-                                height: '50px',
-                                lineHeight: '50px',
-                            }}
+                        <div className="logo" />
+                        <Menu
+                            theme="dark"
+                            defaultSelectedKeys={['/']}
+                            mode="inline"
                         >
-                            <Link to="/today">
-                                <SmileOutlined />
-                                <span>할일</span>
-                            </Link>
-                        </Menu.Item>
-                        <Menu.Item
-                            key="2"
-                            style={{
-                                margin: '0px',
-                                height: '50px',
-                                lineHeight: '50px',
-                            }}
-                        >
-                            <Link to="/star">
-                                <StarOutlined />
-                                <span>중요</span>
-                            </Link>
-                        </Menu.Item>
-                        <Menu.Item
-                            key="3"
-                            style={{
-                                margin: '0px',
-                                height: '50px',
-                                lineHeight: '50px',
-                            }}
-                        >
-                            <Link to="/list">
-                                <CopyOutlined />
-                                <span>제목없는 목록</span>
-                            </Link>
-                        </Menu.Item>
-                    </Menu>
-                </Sider>
-                <Layout className="site-layout">
-                    <div className="App">
-                        <Route
-                            path={['/', '/today']}
-                            component={TodoListTemplate}
-                            p
-                            exact={true}
-                        />
-                        <Route
-                            path="/star"
-                            component={TodoStarItem}
-                            exact={true}
-                        />
-                        <Route
-                            path="/list"
-                            component={TodoTemplate}
-                            exact={true}
-                        />
-                    </div>
+                            {allDatas.map((list) => (
+                                <Menu.Item
+                                    key={list.key}
+                                    style={{
+                                        margin: '0px',
+                                        height: '50px',
+                                        lineHeight: '50px',
+                                    }}
+                                >
+                                    <Link to={list.key}>{list.title}</Link>
+                                </Menu.Item>
+                            ))}
+                        </Menu>
+                    </Sider>
+                    <Layout className="site-layout">
+                        <div className="App">
+                            <Switch>
+                                <Route
+                                    path="/"
+                                    exact
+                                    children={
+                                        <TodoListTemplate
+                                            allDatas={allDatas}
+                                            saveAll={saveAll}
+                                        />
+                                    }
+                                />
+                                <Route
+                                    path="/:id"
+                                    children={
+                                        <TodoListTemplate
+                                            allDatas={allDatas}
+                                            saveAll={saveAll}
+                                        />
+                                    }
+                                />
+                            </Switch>
+                        </div>
+                    </Layout>
                 </Layout>
-            </Layout>
-        </ColorProvider>
+            </ColorProvider>
+        </Router>
     );
 }
 
--- src/components/TodoListTemplate.js
+++ src/components/TodoListTemplate.js
@@ -11,17 +11,12 @@ import SelectColor from './SelectColor';
 import TodoInsert from './TodoInsert';
 import TodoList from './TodoList';
 import moment from 'moment';
-import { remove, set } from 'immutable';
 
 //TODO console 에러 잡기
-const TodoListTemplate = () => {
+const TodoListTemplate = ({ allDatas, saveAll }) => {
+    const { id } = useParams();
     const value = useContext(ColorContext);
     const date = moment().format('YYYY[년] MM[월] DD[일]');
-    const [todos, setTodos] = useState(
-        JSON.parse(localStorage.getItem('todos')) || []
-    );
-    const currentTodos = useRef(todos);
-    // const nextId = useRef(1);
 
     // 테마 변경은 전체에 먹혀야함
     const [isShowConfig, setIsShowConfig] = useState(false);
@@ -29,12 +24,26 @@ const TodoListTemplate = () => {
         setIsShowConfig(!isShowConfig);
     }, [isShowConfig]);
 
-    // currentTodos.current 를 사용해서 star,remove,check 를 해야 오류가 안남.
-    // 렌더링 될때매다 특정작업을 수행
+    console.log('id는 ' + id);
+
+    const tempIndex = allDatas.findIndex((todo) => todo.key === id);
+    const todoIndex = tempIndex > -1 ? tempIndex : 0;
+
+    // allDatas의 초기값은 allDatas의 0번째 배열 (즉 할일 목록)
+    const [data, setData] = useState(allDatas[todoIndex]);
+
+    // 현재 index (여기서 id는 key값)
+
     useEffect(() => {
-        currentTodos.current = todos;
-        updateStarList(currentTodos.current);
-    });
+        let todoData = allDatas[todoIndex];
+        setData(todoData);
+
+        console.log(todoIndex);
+        console.log('useEffect 실행');
+        console.log('todoIndex: ' + todoIndex);
+        console.log('init: ' + JSON.stringify(todoData));
+        console.log('data: ' + JSON.stringify(data));
+    }, [id]);
 
     // +new Date() : +를 붙이면 숫자로 만들어줌
     // 공백이면 경고창 => TodoInsert.js에서 Context를 사용함
@@ -47,84 +56,90 @@ const TodoListTemplate = () => {
                 checked: false,
                 star: false,
             };
-            localStorage.setItem(
-                'todos',
-                JSON.stringify(currentTodos.current.concat(todo))
-            );
-            setTodos(todos.concat(todo));
-            // nextId.current += 1;
+
+            const result = {
+                ...data,
+                todos: data.todos.concat(todo),
+            };
+
+            setData(result);
+
+            //slice()는 전체 반환 결국 allDatasClone엔  allDatas가 들어있음
+            const allDatasClone = allDatas.slice();
+            // 클론한 데이터를 변경시킴
+            allDatasClone.splice(todoIndex, 1, result);
+            saveAll(allDatasClone);
+
+            console.log(todoIndex);
+            console.log(result);
+            console.log('현재 data: ' + JSON.stringify(data));
+            console.log('현재 입력값은:' + JSON.stringify(todo) + '입력됨');
+            console.log(allDatas);
         },
-        [todos]
+        [data]
     );
 
     // 원하는 항목 지우는 함수
-    const onRemove = (id) => {
-        // console.log(currentTodos.current);
-        localStorage.setItem(
-            'todos',
-            JSON.stringify(
-                currentTodos.current.filter((todo) => todo.id !== id)
-            )
-        );
-        setTodos(currentTodos.current.filter((todo) => todo.id !== id));
-    };
+    const onRemove = useCallback(
+        (id) => {
+            console.log(id);
+            data.todos = data.todos.filter((todo) => todo.id !== id);
+            // setAllDatas(data.todos);
+            localStorage.setItem('allDatas', JSON.stringify(allDatas));
 
-    //check하는 함수 만들기
+            console.log(JSON.stringify(data));
+            console.log(
+                '필터링을 한 결과: ' +
+                    JSON.stringify(data.todos.filter((todo) => todo.id !== id))
+            );
+            // setAllDatas(JSON.stringify(allDatas));
+        },
+        [data]
+    );
+
+    //체크 하는 함수 만들기
     const onToggle = useCallback(
         (id) => {
             localStorage.setItem(
-                'todos',
+                'allDatas',
                 JSON.stringify(
-                    currentTodos.current.map((todo) =>
+                    allDatas.todos.map((todo) =>
                         todo.id === id
                             ? { ...todo, checked: !todo.checked }
                             : todo
                     )
                 )
             );
-            setTodos(
-                currentTodos.current.map((todo) =>
-                    todo.id === id ? { ...todo, checked: !todo.checked } : todo
-                )
-            );
+            // setAllDatas(JSON.stringify(allDatas));
         },
-        [todos]
+        [data]
     );
 
-    // 별 클릭시 색상 변경 & 그와 동시에 그 todo는 중요 라우터에 복사된다.
+    // 별 클릭시 색상 변경
     const onToggleStar = useCallback(
         (id) => {
             localStorage.setItem(
-                'todos',
+                'allDatas',
                 JSON.stringify(
-                    currentTodos.current.map((todo) =>
+                    allDatas.todos.map((todo) =>
                         todo.id === id ? { ...todo, star: !todo.star } : todo
                     )
                 )
             );
-            setTodos(
-                currentTodos.current.map((todo) =>
-                    todo.id === id ? { ...todo, star: !todo.star } : todo
-                )
-            );
+            // setAllDatas(JSON.stringify(allDatas));
         },
-        [todos]
+        [data]
     );
-    // star가 true일 경우 localStorage 의 stars 에 넣는 함수
-    // TODO 함수명 바꾸기
-    const updateStarList = (todos) => {
-        localStorage.setItem(
-            'stars',
-            JSON.stringify(todos.filter((todo) => todo.star === true))
-        );
-    };
 
     // star로 sort()하는법
-    const onTodoSort = useCallback((a, b) => {
-        let starA = a.star ? 0 : 1;
-        let starB = b.star ? 0 : 1;
-        return starA - starB;
-    }, []);
+    const onTodoSort = useCallback(
+        (a, b) => {
+            let starA = a.star ? 0 : 1;
+            let starB = b.star ? 0 : 1;
+            return starA - starB;
+        },
+        [data]
+    );
 
     return (
         <div
@@ -138,13 +153,13 @@ const TodoListTemplate = () => {
                 isShowConfig={isShowConfig}
                 onClickThem={onClickThem}
             />
-            <div className="title">할 일</div>
+            <div className="title">{data.title}</div>
 
             <div className="contents">
                 <TodoInsert onInsert={onInsert} />
                 <TodoList
                     className="TodoList"
-                    todos={todos}
+                    todos={data.todos}
                     onRemove={onRemove}
                     onToggle={onToggle}
                     onToggleStar={onToggleStar}
